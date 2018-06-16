e9b8b1f8a168d756e65d58f543a5db6c32415522
Juri <23640912+jthommen@users.noreply.github.co>
2018-06-16 14:22:31 +0100

    Initial commit

---
+++ 02-hello_world/hello_world.cpp
@@ -0,0 +1,8 @@
+#include "../std_lib_facilities.h"
+
+int main()  // C++ programs start by executing the function main
+{
+    cout << "Hello World!\n";    // output "Hello World!"
+    keep_window_open(); // wait for character to be entered, needed on Windows
+    return 0;
+}
